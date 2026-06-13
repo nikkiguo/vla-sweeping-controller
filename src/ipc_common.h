050b17eba9215bd2d96ca53307eb8b00af77a90a
nikkiguo <nikkiguo1@gmail.com>
2026-06-13 12:04:40 -0400

    extend IPC struct with fields for vla

--- src/ipc_common.h
+++ src/ipc_common.h
@@ -9,5 +9,10 @@ struct SharedDataStruct {
     double joint_pos[6];
     double joint_vel[6];
     uint8_t camera_pixels[640 * 480 * 3];
+
+    double action_target[3]; // Controller EE target (action label)
+    double ee_pos[3]; // Current EE position
+    uint32_t episode_id;
+    uint8_t episode_success;
 };
 #pragma pack(pop)
